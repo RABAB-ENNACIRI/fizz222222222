3561b56c77876051710e469cdc4f0955d26c853d
RABAB-ENNACIRI <rababennaciri2002@gmail.com>
2024-12-19 06:22:07 +0100

    2eme commit

--- src/test/java/FizzBuzzTest/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest/FizzBuzzTest.java
@@ -10,4 +10,11 @@ public class FizzBuzzTest {
         int result = fb.add(2,3);
         assertEquals(5, result, "2 + 3 doit être égal à 5");
     }
+
+    @Test
+    public void fizzBuzzTest3() {
+        FizzBuzz fb = new FizzBuzz();
+        int result = fb.add(3,3);
+        assertEquals(6, result, "2 + 3 doit être égal à 5");
+    }
 }
